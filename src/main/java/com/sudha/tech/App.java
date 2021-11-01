1312348322b010bdecaa719fde57a6612754ed77
balu <balu123@gmail.com>
2021-11-01 18:52:25 +0530

    message

---
+++ src/main/java/com/sudha/tech/App.java
@@ -0,0 +1,13 @@
+package com.sudha.tech;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
